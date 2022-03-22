c4d9d6cfec071f4604aac146c343dbc19d6f741d
Idanise <daniseiheme@gmail.com>
2022-03-22 02:47:56 +0100

    loan functionality

--- script.js
+++ script.js
@@ -176,6 +176,22 @@ btnTransfer.addEventListener('click', function (e) {
   }
 })
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  const amount = Number(inputLoanAmount.value)
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //Add movement
+    currentAccount.movements.push(amount)
+
+    //update UI
+    updateUI(currentAccount)
+  }
+  //Clear input field
+  inputLoanAmount.value = ''
+})
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault()
   if (
