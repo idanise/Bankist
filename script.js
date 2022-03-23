09319ef42ab3f7347b2cb0243a0dc0af442dbd3f
Idanise <daniseiheme@gmail.com>
2022-03-23 01:59:08 +0100

    formatting date on locale

--- script.js
+++ script.js
@@ -13,17 +13,17 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z'
+    '2022-03-22T21:31:17.178Z',
+    '2022-03-18T07:42:02.383Z',
+    '2021-12-28T09:15:04.904Z',
+    '2022-03-01T10:17:24.185Z',
+    '2021-12-31T14:11:59.604Z',
+    '2022-02-27T17:01:17.194Z',
+    '2022-03-19T23:36:17.929Z',
+    '2022-03-20T10:51:36.790Z'
   ],
   currency: 'EUR',
-  locale: 'pt-PT' // de-DE
+  locale: 'en-NG' // de-DE
 }
 
 const account2 = {
@@ -33,14 +33,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z'
+    '2022-03-01T13:15:33.035Z',
+    '2022-03-22T09:48:16.867Z',
+    '2022-01-25T06:04:23.907Z',
+    '2022-02-25T14:18:46.235Z',
+    '2022-02-05T16:33:06.386Z',
+    '2022-03-10T14:43:26.374Z',
+    '2022-03-25T18:49:59.371Z',
+    '2022-03-26T12:01:20.894Z'
   ],
   currency: 'USD',
   locale: 'en-US'
@@ -74,6 +74,28 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount')
 const inputCloseUsername = document.querySelector('.form__input--user')
 const inputClosePin = document.querySelector('.form__input--pin')
 
+//////////////////////////////////
+//functions
+const formatMovementDate = function (date, locale) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24))
+
+  const daysPassed = calcDaysPassed(new Date(), date)
+  console.log(daysPassed)
+
+  if (daysPassed === 0) return 'Today'
+  if (daysPassed === 1) return 'Yesterday'
+  if (daysPassed <= 7) return `${daysPassed} days ago`
+  else {
+    // const day = `${date.getDate()}`.padStart(2, 0)
+    // const month = `${date.getMonth() + 1}`.padStart(2, 0)
+    // const year = date.getFullYear()
+    // return `${day}/${month}/${year} `
+
+    return new Intl.DateTimeFormat(locale).format(date)
+  }
+}
+
 const displayMovments = function (acc, sort = false) {
   //Empties the container
   containerMovements.innerHTML = ''
@@ -86,10 +108,7 @@ const displayMovments = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
 
     const date = new Date(acc.movementsDates[i])
-    const day = `${date.getDate()}`.padStart(2, 0)
-    const month = `${date.getMonth() + 1}`.padStart(2, 0)
-    const year = date.getFullYear()
-    const displayDate = `${day}/${month}/${year} `
+    const displayDate = formatMovementDate(date, acc.locale)
 
     const html = `
     <div class="movements__row">
@@ -153,6 +172,8 @@ const updateUI = function (acc) {
 
 //Event handler
 let currentAccount
+
+//Fake login
 currentAccount = account1
 updateUI(currentAccount)
 containerApp.style.opacity = 100
@@ -178,12 +199,29 @@ btnLogin.addEventListener('click', function (e) {
 
     //Create current date and time
     const now = new Date()
-    const day = `${now.getDate()}`.padStart(2, 0)
-    const month = `${now.getMonth() + 1}`.padStart(2, 0)
-    const year = now.getFullYear()
-    const hour = `${now.getHours()}`.padStart(2, 0)
-    const min = `${now.getMinutes()}`.padStart(2, 0)
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`
+    //Experimenting api
+
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+      weekday: 'short'
+    }
+
+    // const locale = navigator.language
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now)
+    // const day = `${now.getDate()}`.padStart(2, 0)
+    // const month = `${now.getMonth() + 1}`.padStart(2, 0)
+    // const year = now.getFullYear()
+    // const hour = `${now.getHours()}`.padStart(2, 0)
+    // const min = `${now.getMinutes()}`.padStart(2, 0)
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`
 
     //Clear input fields
     inputLoginUsername.value = inputLoginPin.value = ''
